6ba7b59f7046b66c2c29897f3ec5dd89d3dfa762
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-09-07 12:03:46 +0300

    Created BooksServiceTests, fix the problem

---
+++ AspNetSandbox.Tests/BooksServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetSandbox
+{
+    public class BooksServiceTests
+    {
+        [Fact]
+        public void BooksServiceTest()
+        {
+            // Assume
+            var booksService = new BooksService();
+
+            // Act
+            booksService.Post(new Book
+            {
+                Title = "Test Book Nr.1",
+                Author = "Jack 1",
+                Language = "English"
+            });
+            booksService.Delete(2);
+            booksService.Post(new Book
+            {
+                Title = "Test Book Nr.2",
+                Author = "Jack 2",
+                Language = "English"
+            });
+
+            // Assert
+            Assert.Equal("Test Book Nr.2", booksService.Get(3).Title);
+        }
+    }
+}
