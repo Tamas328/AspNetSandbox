d038699feff6c926e5521d0fe174a7586732ab33
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-09-16 12:25:15 +0300

    Added PurchasePrice migration

--- AspNetSandbox/Models/Book.cs
+++ AspNetSandbox/Models/Book.cs
@@ -15,6 +15,6 @@ namespace AspNetSandbox.Models
 
         public string Language { get; set; }
 
-        public double Price { get; set; }
+        public decimal PurchasePrice { get; set; }
     }
 }
