0f9e8d117933c1572a45a51beafef83e2a8910c3
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-09-17 10:45:28 +0300

    Beauty code

--- AspNetSandbox/Startup.cs
+++ AspNetSandbox/Startup.cs
@@ -87,7 +87,7 @@ namespace AspNetSandbox
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
 
