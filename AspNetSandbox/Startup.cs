6060728d537b3481e7c762c5a3385cf6fae96f64
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-09-17 09:56:14 +0300

    Created test for Startup.

--- AspNetSandbox/Startup.cs
+++ AspNetSandbox/Startup.cs
@@ -64,7 +64,7 @@ namespace AspNetSandbox
             return Configuration.GetConnectionString("DefaultConnection");
         }
 
-        private string ConvertConnectionString(string connectionString)
+        public static string ConvertConnectionString(string connectionString)
         {
             throw new NotImplementedException();
         }
---
+++ BooksServiceTests/StartupTests.cs
@@ -0,0 +1,26 @@
+﻿using AspNetSandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BooksServiceTests
+{
+    public class StartupTests
+    {
+        [Fact]
+        public void CheckConversionToEfConnectionString()
+        {
+            // Assume
+            string databaseUrl = "postgres://ahcuwuuytkwcex:ca6592f21955b266e2f2cebd75ef2117a438314beb66fe1c93bc58bc8ff95c95@ec2-52-214-178-113.eu-west-1.compute.amazonaws.com:5432/d4ne7vk964ctvm";
+
+            // Act
+            string convertedConnectionString = Startup.ConvertConnectionString(databaseUrl);
+
+            // Assert
+            Assert.Equal("Port=5432; Database=d4ne7vk964ctvm; Host=ec2-52-214-178-113.eu-west-1.compute.amazonaws.com; User Id=ahcuwuuytkwcex; Password=ca6592f21955b266e2f2cebd75ef2117a438314beb66fe1c93bc58bc8ff95c95; SSL Mode=Require; Trust Server Certificate=true", convertedConnectionString);
+        }
+    }
+}
