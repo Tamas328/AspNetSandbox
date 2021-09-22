a73e4397389c4002d0078f1b754dcd26d18d6776
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-09-22 17:14:17 +0300

    Addign Command Line Parser

--- AspNetSandbox/Program.cs
+++ AspNetSandbox/Program.cs
@@ -11,9 +11,20 @@ namespace AspNetSandbox
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"There are {args.Length} args.");
+            }
+            else
+            {
+                Console.WriteLine("There are no args.");
+            }
+
             CreateHostBuilder(args).Build().Run();
+
+            return 0;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
