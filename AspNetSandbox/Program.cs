ee741d6877f05e44b9aaa59f7ef5c9674cf7fe10
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-09-22 19:02:05 +0300

    Created FileOperationWithBooksTest

--- AspNetSandbox/Program.cs
+++ AspNetSandbox/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandLine;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,8 +12,29 @@ namespace AspNetSandbox
 {
     public class Program
     {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+
         public static int Main(string[] args)
         {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"There are {args.Length} args.");
---
+++ BooksServiceTests/FileOperationWithBooksTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BooksServiceTests
+{
+    public class FileOperationWithBooksTest
+    {
+        [Fact]
+        public void EnumerateFilesTest()
+        {
+            System.IO.DirectoryInfo directoryInfo = new System.IO.DirectoryInfo(".");
+            var files = directoryInfo.EnumerateFiles();
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+        }
+
+        [Fact]
+        public void CreateFileTest()
+        {
+            File.WriteAllText("newSettings.json", @"{
+  ""ConnectionStrings"": {
+    ""DefaultConnection"": ""Port=5432; Database=SandboxDb; Host=localhost; User Id=postgres; Trust Server Certificate=true"",
+    ""Heroku"": ""Port=5432; Database=d4ne7vk964ctvm; Host=ec2-52-214-178-113.eu-west-1.compute.amazonaws.com; User Id=ahcuwuuytkwcex; Password=ca6592f21955b266e2f2cebd75ef2117a438314beb66fe1c93bc58bc8ff95c95; SSL Mode=Require; Trust Server Certificate=true""
+  },
+  ""Logging"": {
+    ""LogLevel"": {
+      ""Default"": ""Information"",
+      ""Microsoft"": ""Warning"",
+      ""Microsoft.Hosting.Lifetime"": ""Information""
+    }
+  },
+  ""AllowedHosts"": ""*""
+}
+");
+        }
+
+        [Fact]
+        public void ReadFilesTest()
+        {
+            using (FileStream fs = File.OpenRead("newSettings.json"))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    var returnedString = temp.GetString(b);
+                    Console.WriteLine(returnedString);
+                }
+            }
+        }
+    }
+}
